0a621adf025ae6c27a70abe18c02d056ec7301c2
Tanel Ristsalu <tanel.ristsalu@tktk.ee>
2024-02-18 19:29:50 +0200

    week2 code

---
+++ OddOrEven/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Enter a number:");
+int userNum = Convert.ToInt32(Console.ReadLine());
+
+int result = userNum % 2;
+
+if (result != 0)
+{
+    Console.WriteLine("Number is odd");
+} else
+{
+    Console.WriteLine("Number is even.");
+}
---
+++ Week3/Program.cs
@@ -0,0 +1,8 @@
+﻿// % näitab jääki peale jagamist
+
+int a = 25;
+int b = 8;
+
+int c = a % b;
+
+Console.WriteLine(c);
